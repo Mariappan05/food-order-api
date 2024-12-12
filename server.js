2b09ca99e32c36eec9f4463b966024a829cc2415
Mariappan05 <rammariappan05@gmail.com>
2024-12-12 16:51:17 +0530

    wfww

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 // Create a MySQL connection
 const db = mysql.createConnection({
-  host: 'food-order-api.onrender.com',
+  host: 'food_order.onrender.com',
   user: 'root', // Your MySQL username
   password: 'madan@2004', // Your MySQL password
   database: 'food_order',
